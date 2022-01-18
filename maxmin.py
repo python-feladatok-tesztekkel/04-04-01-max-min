311a7a5e5ca620a6274a203e7275e67cf5997a47
Gyuris Csaba <gycsaba72@gmail.com>
2022-01-18 09:42:51 +0100

    Update maxmin.py

--- maxmin.py
+++ maxmin.py
@@ -7,7 +7,7 @@ def feladat01() -> int:
     return 0
 
 # 2. feladat
-# Melyik a legkisebb elem:
+# Melyik a legkisebb elem?
 def feladat02() -> int:
     szamok = [-21, 15, 4, -1, 3, -3, 51, 11, -41, 51, 8, 31, -11, 9, -4, 9, -2, -10]
     return 0
